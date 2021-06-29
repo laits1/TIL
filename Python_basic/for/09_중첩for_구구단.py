b37440fe74fce0326f6e818aa6fe0a4a04567762
laits1 <thsehdrl1@gmail.com>
2021-06-29 10:33:23 +0900

    2021년 06월 29일 화 오전 10:33:23

---
+++ Python_basic/for/09_중첩for_구구단.py
@@ -0,0 +1,6 @@
+# 구구단 출력
+
+for i in range(1,10):
+    for j in range(1,10):
+        print(f"{i}*{j}={i*j}")
+    print("")
--- Python_basic/for/for_ex5.py
+++ Python_basic/for/for_ex5.py
@@ -3,7 +3,9 @@
 # 1 2 3 4
 # 5 6 7 8
 # 9 10 11 12
+a = 1
 for i in range(3):
     for j in range(4):
-        print(f"{j} ", end=" ")
+        print(f"{a} ", end=" ")
+        a +=1
     print(" ")
