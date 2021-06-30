afda7406cb3241160d62b0b98d05d8094cc5183b
laits1 <thsehdrl1@gmail.com>
2021-06-30 11:08:23 +0900

    .

--- Python_basic/for/for_ex6.py
+++ Python_basic/for/for_ex6.py
@@ -17,11 +17,13 @@ print("--------------------------")
 for i in range(1,6):
     for j in range(5-i):
         print(" ", end="")
-    for j in range(1,i*2,1):
+    for j in range(1,i*2):
         print("*", end="")
     print()
 print("--------------------------")
 for i in range(1,6):
-    for j in range(1,5-i):
+    for j in range(i-1):
+        print(" ", end="")
+    for j in range(11,i*2,-1):
         print("*", end="")
     print()
