58b9b32df06b565a09b4b50dda1010168eb3f20c
laits1 <thsehdrl1@gmail.com>
2021-07-21 21:55:49 +0900

    2021년 07월 21일 수 오후  9:55:49

---
+++ Python_basic/class_object/02_class2_생성자구현.py
@@ -0,0 +1,40 @@
+# 클래스 정의
+# name, age 2개의 속성을 정의 - 기본값을 저장
+
+class person :
+    # 생성자 함수
+    def __init__(self) :
+        self.name = 'Kate'
+        self.age = 10
+        print(self, 'is generated')
+
+# width, height 2개의 속성을 정의 - 기본값을 저장
+class Rectangle :
+    # 생성자 함수
+    def __init__(self) :
+        self.width = 1
+        self.height = 1
+
+
+# person 클래스 객체 생성
+member1 = person()
+member2 = person()
+
+print("=== attribute 값 변경 전 ===")
+print(member1.name, member1.age)
+print(member2.name, member2.age)
+
+member1.name = "kim"
+member2.age = 25
+
+print("=== attribute 값 변경 후 ===")
+print(member1.name, member1.age)
+print(member2.name, member2.age)
+
+# Rectangle 클래스 객체 생성
+rect1 = Rectangle()
+rect2 = Rectangle()
+
+print(rect1.width, rect1.height)
+print(rect2.width, rect2.height)
+
