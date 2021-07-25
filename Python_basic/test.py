246635aa701689273970ba87b052d18764f7976c
laits1 <thsehdrl1@gmail.com>
2021-07-25 12:08:10 +0900

    2021년 07월 25일 일 오후 12:08:10

--- Python_basic/test.py
+++ Python_basic/test.py
@@ -1,6 +1,6 @@
-list = [1,2,3,
-        4,5,6,
-        7,8,9]
+ a, b = map(int, input().strip().split(' '))
 
-print(list)
-
+for i in range(b) :
+    for j in range(a) :
+        print("*",end="")
+    print("")
