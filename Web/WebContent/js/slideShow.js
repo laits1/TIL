2e2901a00b9fa317ad5ffdf7fae5ced4d3698f9c
laits1 <thsehdrl1@gmail.com>
2021-08-03 17:56:35 +0900

    2021년 08월  3일 화 오후  5:56:35

--- Web/WebContent/js/slideShow.js
+++ Web/WebContent/js/slideShow.js
@@ -2,16 +2,16 @@
 
 $(function() {
     // 이동한 이미지의 index 값 저장 (현재 화면에서 보여지는 이미지의 index)
-    var moveIndex = 0;
+    var movedIndex = 0;
 
     // 슬라이드 패널을 움직여주는 함수
 
     function moveSlide(index) {
         // 전달받은 index 값을 moveIndex에 저장
-        moveIndex = index;
+        movedIndex = index;
         
         // 슬라이드 이동
-        var moveLeft = (index * 1280);
+        var moveLeft =- (index * 1280);
         $('#slidePanel').animate({'left':moveLeft}, 'slow')
     }
 
@@ -31,5 +31,23 @@ $(function() {
         moveSlide(movedIndex); //인덱스값 전달			
     });
 
+    $('.controlButton').each(function(index) {
+        $(this).hover(
+            function() {
+                $(this).attr('src', 'image/controlButton2.png');
+            },
+            function() {
+                $(this).attr('src', 'image/controlButton1.png');
+            }
+        );      // hover 종료
+    // 클릭 하면 현재 인덱스 값을 moveSlide() 함수에 전달
+        $(this).on('click', function() {
+            moveSlide(index);
+            
+        });
+
+    });     // each 끝
+
+    
 
 });
