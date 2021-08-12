436b8cd4d8f7d000f40b3b546a22ae028684dce4
laits1 <thsehdrl1@gmail.com>
2021-08-12 17:58:19 +0900

    2021년 08월 12일 목 오후  5:58:17

--- DB/DJango/ToDoList/ToDolist/my_to_do_app/views.py
+++ DB/DJango/ToDoList/ToDolist/my_to_do_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 
@@ -12,5 +13,8 @@ def index(request) :
 def createTodo(request) :
     # 사용자가 메모에 입력해서 넘긴 값을 반환하는 코드
     user_input_str = request.POST['todoContent']
+    new_todo = Todo(content=user_input_str) # insert into todo(content) values (input_str)
+    new_todo.save() # db에 반영
+
     return HttpResponse("입력한 메모 data는 : " + user_input_str)
     # return HttpResponse("create toDo 메모 작성 ")
