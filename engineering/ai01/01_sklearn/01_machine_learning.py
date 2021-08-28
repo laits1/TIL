6113217366adf83f919b99dd2f62a1c7dca259e5
laits1 <thsehdrl1@gmail.com>
2021-08-28 17:28:29 +0900

    2021년 08월 28일 토 오후  5:28:28

--- engineering/ai01/01_sklearn/01_machine_learning.py
+++ engineering/ai01/01_sklearn/01_machine_learning.py
@@ -7,14 +7,18 @@ import matplotlib.pyplot as plt
 X = np.array([1, 2, 3, 4, 5])
 y = np.array([2, 4, 6, 8, 10])
 
-X = X.reshape(-1, 1)
 # print(X)
+X = X.reshape(-1, 1)
+print(X)
+
 linear = LinearRegression()
+
 linear.fit(X, y)
 
 test_X = np.array([6, 7, 8, 9, 10])
 predict = linear.predict(test_X.reshape(-1, 1))
 
 print(predict)
+print(linear.predict([[23]]))
 plt.plot(test_X, predict)
 plt.show()
