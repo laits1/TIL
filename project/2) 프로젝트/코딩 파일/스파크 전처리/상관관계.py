87a2c23942da6f8d5c82f6069eff55a1fea98740
laits1 <thsehdrl1@gmail.com>
2021-10-01 11:06:33 +0900

    2021년 10월  1일 금 오전 11:06:33

--- project/2) 프로젝트/코딩 파일/스파크 전처리/상관관계.py
+++ project/2) 프로젝트/코딩 파일/스파크 전처리/상관관계.py
@@ -23,11 +23,13 @@ for s in stocks + benchmarks:
 
 prc = prc.dropna()
 
-portfolio = prc[['MSFT', 'AMD', 'AMZN', 'KO','MCD','MO','NKE','PFE','WMT']]
-portfolio = prc[['MSFT', 'AMD', 'AMZN', 'KO', 'MCD', 'MO', 'NKE', 'PFE', 'WMT']].pct_change()
+portfolio = prc[['MSFT', 'AMD', 'AMZN', 'KO','MCD','MO','NKE','PFE','WMT','SPY','SHV']]
+portfolio = prc[['MSFT', 'AMD', 'AMZN', 'KO', 'MCD', 'MO', 'NKE', 'PFE', 'WMT','SPY','SHV']].pct_change()
 portfolio_data = 1 +  portfolio
 portfolio_data.loc[pd.to_datetime("2007-01-11")] = 1
 portfolio_data = portfolio_data.sort_index()
 
 portfolio_data = portfolio_data.cumprod()
 portfolio_data.to_csv('./daily.csv')
+portfolio['SPY'].to_csv('./SPY.csv')
+portfolio['SHV'].to_csv('./SHV.csv')
